96a683f23c4ddd4aeb9cb1387da7b08a04a2de2e
Silva Nicolas <nicolassilva1108@gmail.com>
2024-10-14 02:01:42 -0300

    Commit 04

--- src/main/java/com/parcial_prog3/AdnApplication.java
+++ src/main/java/com/parcial_prog3/AdnApplication.java
@@ -2,12 +2,26 @@ package com.parcial_prog3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import com.parcial_prog3.services.DnaAnalysis;
 @SpringBootApplication
 public class AdnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdnApplication.class, args);
+	}
+
+	@Bean
+	public org.springframework.boot.web.server.WebServerFactoryCustomizer<org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory> containerCustomizer(Environment environment) {
+		return factory -> {
+			String port = environment.getProperty("PORT");
+			if (port != null) {
+				factory.setPort(Integer.parseInt(port));
+			} else {
+				factory.setPort(8080); // Default to port 8080 if PORT is not set
+			}
+		};
 	}
 
 }
